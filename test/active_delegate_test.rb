502e184635c84d5e77f48281336bbccd90ec45c5
Jonian Guveli <jonian@hardpixel.eu>
2017-05-31 12:22:04 +0300

    remove dummy test

--- test/active_delegate_test.rb
+++ test/active_delegate_test.rb
@@ -4,8 +4,4 @@ class ActiveDelegateTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ActiveDelegate::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
